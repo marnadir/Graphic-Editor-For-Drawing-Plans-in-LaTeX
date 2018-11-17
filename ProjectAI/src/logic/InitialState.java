654747da163d6e8003b9cb9b95021d1a4745b7f3
nadir <nadir@nadir-desktop>
2018-11-17 01:00:24 +0100

    finish to draw the initial state

--- ProjectAI/src/logic/InitialState.java
+++ ProjectAI/src/logic/InitialState.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.LineAttributes;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridLayout;
@@ -44,7 +45,32 @@ public class InitialState {
 			public void paintControl(PaintEvent e) {
 			
 				int numCond=precPos.size();
-				e.gc.drawRectangle(composite.get, y, width, height);
+				
+				int startX=composite.getLocation().x+40;
+				int startY=composite.getLocation().y+40;
+				
+				
+				e.gc.setLineWidth(6);
+				e.gc.drawLine(startX, startY, startX,startY+(30*numCond));
+				e.gc.setLineWidth(1);
+				
+				
+				int posY=startY+10;
+				for(int i=0;i<numCond;i++) {
+			
+					
+					
+					e.gc.drawLine(startX, posY, startX+50, posY);
+					String string=precPos.get(i);
+					e.gc.drawString(string, startX+10, posY-20,false);
+					
+					posY=posY+30;
+					
+				}
+				
+				
+				
+				//e.gc.drawRectangle(r);
 			}
 		});
 		
